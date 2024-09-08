80a01812230da15297b0ea43d4594f55770b6c63
asifkamal <asifkamal530@gmail.com>
2024-09-08 21:14:04 +0600

    Circui Breaker not working

---
+++ APIGATEWAY_CIRCUITBREAKER/src/main/java/doom/practice/demoMicroservice/DemoMicroserviceApplication.java
@@ -0,0 +1,25 @@
+package doom.practice.demoMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableDiscoveryClient
+public class DemoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroserviceApplication.class, args);
+	}
+
+	/*@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}*/
+}
---
+++ APIGATEWAY_CIRCUITBREAKER/src/main/java/doom/practice/demoMicroservice/Fallback/FallBackController.java
@@ -0,0 +1,24 @@
+package doom.practice.demoMicroservice.Fallback;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallBackController {
+    @GetMapping("/userCircuitBreaker")
+
+    public ResponseEntity<String> userCircuitBreaker(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("We are facing a problem in user. Please contact helpdesk");
+    }
+
+    @GetMapping("/deptCircuitBreaker")
+    public ResponseEntity<String> deptCircuitBreaker(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("We are facing a problem in department. Please contact helpdesk");
+    }
+}
